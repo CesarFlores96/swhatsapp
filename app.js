c75ae6772fc1f1d1e24a2f248dc96d52fffaddec
Cesar Flores <cesarfmiranda0@gmail.com>
2024-12-06 20:38:37 -0500

    Ultimo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port =process.env.PORT || 3000;
 
 // Importar rutas
 const whatsappRoutes = require('./routes/whatsapp');
